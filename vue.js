5aa8f3795ab34331683c9eb8a45e6f523d655edd
sexykitten404 <35409716+sexykitten404@users.noreply.github.com>
2020-02-13 18:08:39 -0500

    Update vue.js

--- vue.js
+++ vue.js
@@ -1,4 +1,13 @@
 katrina.vue
 <template>
+  <p>{{question}} Hello, </p>
 
-  
+<script>
+    module.exports = {
+data.funtion () {
+    return {
+    question: 'how many years old are you?' 
+    }
+  }
+}
+</script> 
