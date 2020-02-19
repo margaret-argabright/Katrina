eac7b4e4e13d604b45d94d143a3fc92c66b4a95a
sexykitten404 <35409716+sexykitten404@users.noreply.github.com>
2020-02-19 06:33:11 -0500

    Update vue.js

--- vue.js
+++ vue.js
@@ -1,29 +1,49 @@
-<form 
-id="app"
-@submit="checkForm"
-action="#backupservervalidation"
-method=post
->
-  <p v-if="errors.length">
-    <b>Please correct the following error(s):</b>
-    <ul>
-      <li v-for="error in errors">{{ error }}</li>
-    </ul>
-  </p>
- <p>
-    <label for="age">How many years old are you?</label>
-    <input
-      id="age"
-      v-model="age"
-      type="text"
-      name="age"
-    >
-  </p>
-     <p>
-    <input
-      type="submit"
-      value="Submit"
-    >
-  </p>
+<template>
+  <div class="form pt-6">
+    <div class="summary text-red" v-if="$v.form.$error">
+      Form has errors
+    </div>
+    <form @submit.prevent="submit">
+      <div class="flex justify-center my-6">
+        <div
+           class="px-4"
+           :class="{ 'hasError': $v.form.name.$error }">
+          <label class="mr-2 font-bold text-grey">Name</label>
+          <input type="text" class="input" v-model="form.name">
+        </div>
+        <div 
+          class="px-4"
+          :class="{ 'hasError': $v.form.age.$error }">
+          <label class="mr-2 font-bold text-grey">How many years old are you?</label>
+          <input type="age" class="input" v-model="form.age">
+        </div>
+      </div>
+      <div class="text-center">
+        <button type="submit" class="button">
+          Submit
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
 
-</form>
+<script>
+import { required, minLength, between } from 'vuelidate/lib/validators'
+ 
+export default {
+  data () {
+    return {
+      age: 0
+    }
+  },
+  validations: {
+    age: {
+      required,
+      minLength: minLength(1)
+    },
+    age: {
+      between: between(1, 120)
+    }
+  }
+}
+</script>
