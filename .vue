03883b1bea256145754b476d6573d1e9b48a1bb8
sexykitten404 <35409716+sexykitten404@users.noreply.github.com>
2020-02-19 06:09:12 -0500

    Create .vue

---
+++ .vue
@@ -0,0 +1,29 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    errors: [],
+    name: ''
+  },
+  methods:{
+    checkForm: function (e) {
+      e.preventDefault();
+
+      this.errors = [];
+
+      if (this.name === '') {
+        this.errors.push('Product name is required.');
+      } else {
+        fetch(apiUrl + encodeURIComponent(this.name))
+        .then(res => res.json())
+        .then(res => {
+          if (res.error) {
+            this.errors.push(res.error);
+          } else {
+            // redirect to a new URL, or do something on success
+            alert('okay!');
+          }
+        });
+      }
+    }
+  }
+})
