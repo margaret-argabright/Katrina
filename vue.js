0d7eeaf9729f0fb290facc3fc1825026211e2920
sexykitten404 <35409716+sexykitten404@users.noreply.github.com>
2020-02-19 06:00:48 -0500

    Update vue.js

--- vue.js
+++ vue.js
@@ -1,21 +1,30 @@
 katrina.vue
-<template>
-  <span>{{ message }}</span>
-</template>
+<form 
+id="app"
+@submit="checkForm"
+action="#backupservervalidation"
+method=post
+>
+  <p v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+    <ul>
+      <li v-for="error in errors">{{ error }}</li>
+    </ul>
+  </p>
+ <p>
+    <label for="age">How many years old are you?</label>
+    <input
+      id="age"
+      v-model="age"
+      type="text"
+      name="age"
+    >
+  </p>
+     <p>
+    <input
+      type="submit"
+      value="Submit"
+    >
+  </p>
 
-<script>
-    export default
-      data () {
-        return {
-          message: 'how many years old are you?'
-    }
-  }
-}
-</script> 
-<style scoped>
-  p {
-    font-size: 2em;
-    text-align: center;
-  }
-</style>
-
+</form>
